25c9d31766b45acdf3ff17fb9d9c277e516d40fb
Cuongdao <cuongdao2692@gmail.com>
2022-02-17 10:07:11 +0700

    sua code

--- app/Http/Controllers/Frontend/filterController.php
+++ app/Http/Controllers/Frontend/filterController.php
@@ -29,7 +29,8 @@ class filterController extends Controller
 
         $keys =  [];
 
-        if(isset($list_data_group)){
+      
+        if(!empty($list_data_group[0]['value'])){
 
             foreach ($list_data_group as $key => $value) {
 
@@ -37,60 +38,68 @@ class filterController extends Controller
 
                     $arr = json_decode($values, true);
 
-                    array_push($fill, $arr);
+                    if(isset($arr)){
 
-                    $keys[] = array_keys($arr);
-                   
+                        array_push($fill, $arr);
+
+                        $keys[] = array_keys($arr);
+                    }
+                    
                 }
                
             }
-        }
 
-        $result = [];
-        $product = [];
+            $result = [];
+            $product = [];
 
+            if(isset($keys)){
 
-        foreach($keys as $key1 => $vals){
+                foreach($keys as $key1 => $vals){
 
-           
-            foreach($vals as $valu){
+               
+                    foreach($vals as $valu){
 
-                if(in_array($valu, $property)){
+                        if(in_array($valu, $property)){
 
-                    $result[] = $fill[$key1][$valu];
+                            $result[] = $fill[$key1][$valu];
+                        }
+                    
+                    }
+
                 }
+                
+                if(isset($result)){
+
+                    foreach ($result as  $res) {
+                        foreach($res as $res1){
+                            $product[] = $res1;
+                        }
+                    }
+                }
+                
+                $number_key = count($keys);
+
+                $number_product    = array_count_values($product);
+
+                
+                if(isset($number_product)){
+                    $result_product = [];
+                    foreach ($number_product as $key => $value) {
+                        if($value == $number_key){
+                            array_push($result_product, $key);
+                        }
+                    }
+                }
+
+                $product_search = product::whereIn('id', $result_product)->get();
+
+                return view('frontend.ajax.product', compact('product_search'));
+
+            }
             
-            }
 
-        }
-        
-        if(isset($result)){
-
-            foreach ($result as  $res) {
-                foreach($res as $res1){
-                    $product[] = $res1;
-                }
-            }
-        }
-        
-        $number_key = count($keys);
-
-        $number_product    = array_count_values($product);
-
-        
-        if(isset($number_product)){
-            $result_product = [];
-            foreach ($number_product as $key => $value) {
-                if($value == $number_key){
-                    array_push($result_product, $key);
-                }
-            }
-        }
-
-        $product_search = product::whereIn('id', $result_product)->get();
-
-        return view('frontend.ajax.product', compact('product_search'));
-
+        }    
+            
     }
     
 }

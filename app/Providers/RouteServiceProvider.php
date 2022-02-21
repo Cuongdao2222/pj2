d5d977df553d788a20bf36a549b8e7979e43892b
Cuongdao <cuongdao2692@gmail.com>
2022-02-21 11:54:24 +0700

    sua code

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -21,7 +21,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/admins/products';
+    public const HOME = '/admins/home';
 
     /**
      * Define your route model bindings, pattern filters, etc.

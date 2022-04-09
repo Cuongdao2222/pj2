5b0ff514a15bcd8a3083d5d935756ccad82b6696
Cuongdao <cuongdao2692@gmail.com>
2022-04-09 10:25:44 +0700

    sua code

--- app/lib/Lib/Alepay.php
+++ app/lib/Lib/Alepay.php
@@ -236,9 +236,7 @@ class Alepay {
         $signature = $this->alepayUtils->makeSignature($data, $this->checksumKey);
         $data['signature'] = $signature;
         $data_string = json_encode($data);
-		$url =  $this->baseURL[$this->env] . $this->URI['requestPayment'];
-
-        print_r($url);
+		$url =  $this->baseURL['test'] . $this->URI['requestPayment'];
 
         $ch = curl_init($url);
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
